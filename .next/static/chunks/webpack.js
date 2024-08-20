ad5d4135b51d6c7bb4a0a35acc60759600e11fef
Ayat UR Rehman <48249641+ayaturrehman@users.noreply.github.com>
2024-08-20 08:00:26 +0100

    menu fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c538acedbcc6b19"; }
+/******/ 		__webpack_require__.h = function() { return "23d021b81653033b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
