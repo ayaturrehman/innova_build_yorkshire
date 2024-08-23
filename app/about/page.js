34a112cf00844f0773aa06b6f9804e6e6e7630d4
Ayat UR Rehman <48249641+ayaturrehman@users.noreply.github.com>
2024-08-23 21:28:44 +0100

    menu fix

--- app/about/page.js
+++ app/about/page.js
@@ -4,7 +4,6 @@ import CounterUp from "@/components/elements/CounterUp"
 import Link from "next/link"
 import { useState } from 'react'
 
-
 export default function About() {
     const [isActive, setIsActive] = useState({
         status: false,
